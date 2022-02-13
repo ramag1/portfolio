f34750fe192d99f165c9c9a8831a2416585f7cc7
rmagley <rachelmagley@gmail.com>
2022-02-12 22:36:26 -0500

    Add styling to mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ function Home(props) {
 	return (
         <div>
 		    <h2>Welcome to my journey!</h2>
-			<p>Full Stack Engineer <br />
-			Outdoor Enthusiast <br />
-			Underwater Explorer</p>
+			<p>Equal parts Full Stack Engineering & Underwater Exploring.</p>
 		</div>
 	);
 }
