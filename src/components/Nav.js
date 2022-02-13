dea365fee92c3dbed269afd6e7998617a78ae20f
rmagley <rachelmagley@gmail.com>
2022-02-12 21:15:47 -0500

    Add menu icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { Twirl as Hamburger } from 'hamburger-react';
-// import Hamburger from './Hamburger';
-// import './Hamburger.css';
+import Hamburger from 'hamburger-react';
+import { FiMenu } from 'react-icons/fi';
 import './Nav.css';
 // import { MdClose } from 'react-icons/md';
 // import { FiMenu } from 'react-icons/fi';
 
 function Nav(props) {
 	const [navbarOpen, setNavbarOpen] = useState(false);
-    // const handleToggle = () => {
-	// 	setNavbarOpen((prev) => !prev);
-	// 	};
+
     const handleToggle = () => {
 			setNavbarOpen(!navbarOpen);
 		};
@@ -21,7 +18,7 @@ function Nav(props) {
 
 	return (
 		<nav className='navBar'>
-			<button onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
+			<button className='FiMenu' onClick={handleToggle}><FiMenu/></button>
 			<ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
 				<li>
 					<Link
@@ -74,3 +71,60 @@ function Nav(props) {
 }
 
 export default Nav;
+
+
+// 	return (
+// 		<nav className='navBar'>
+// 			<button className='FiMenu' onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
+// 			<ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+// 				<li>
+// 					<Link
+// 						to='/'
+// 						activeClassName='active-link'
+// 						onClick={() => closeMenu()}
+// 						exact>
+// 						Home
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						to='/about'
+// 						activeClassName='active-link'
+// 						onClick={() => closeMenu()}
+// 						exact>
+// 						About
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						to='/projects'
+// 						activeClassName='active-link'
+// 						onClick={() => closeMenu()}
+// 						exact>
+// 						Projects
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						to='/skills'
+// 						activeClassName='active-link'
+// 						onClick={() => closeMenu()}
+// 						exact>
+// 						Skills
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						to='/resume'
+// 						activeClassName='active-link'
+// 						onClick={() => closeMenu()}
+// 						exact>
+// 						Resume
+// 					</Link>
+// 				</li>
+// 			</ul>
+// 		</nav>
+// 	);
+// }
+
+// export default Nav;
