89bdffaf07eb170437e1e0096af397f19c3290c4
rmagley <rachelmagley@gmail.com>
2022-02-12 21:05:45 -0500

    Add functionality to close menu automatically

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 // import Hamburger from './Hamburger';
 // import './Hamburger.css';
 import './Nav.css';
+// import { MdClose } from 'react-icons/md';
+// import { FiMenu } from 'react-icons/fi';
 
 function Nav(props) {
 	const [navbarOpen, setNavbarOpen] = useState(false);
@@ -13,25 +15,58 @@ function Nav(props) {
     const handleToggle = () => {
 			setNavbarOpen(!navbarOpen);
 		};
+    const closeMenu = () => {
+			setNavbarOpen(false);
+		};
 
 	return (
 		<nav className='navBar'>
 			<button onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
 			<ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
 				<li>
-					<Link to='/'>Home</Link>
+					<Link
+						to='/'
+						activeClassName='active-link'
+						onClick={() => closeMenu()}
+						exact>
+						Home
+					</Link>
 				</li>
 				<li>
-					<Link to='/about'>About</Link>
+					<Link
+						to='/about'
+						activeClassName='active-link'
+						onClick={() => closeMenu()}
+						exact>
+						About
+					</Link>
 				</li>
 				<li>
-					<Link to='/projects'>Projects</Link>
+					<Link
+						to='/projects'
+						activeClassName='active-link'
+						onClick={() => closeMenu()}
+						exact>
+						Projects
+					</Link>
 				</li>
 				<li>
-					<Link to='/skills'>Skills</Link>
+					<Link
+						to='/skills'
+						activeClassName='active-link'
+						onClick={() => closeMenu()}
+						exact>
+						Skills
+					</Link>
 				</li>
 				<li>
-					<Link to='/resume'>Resume</Link>
+					<Link
+						to='/resume'
+						activeClassName='active-link'
+						onClick={() => closeMenu()}
+						exact>
+						Resume
+					</Link>
 				</li>
 			</ul>
 		</nav>
