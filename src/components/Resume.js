b2931b7484d7934ea93ca250652fbe8bf12a97eb
rmagley <rachelmagley@gmail.com>
2022-02-17 02:08:30 -0500

    Add Resume download link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Resume(props) {
-	return <div>Resume</div>;
+	return (
+		<div>
+			<h2>Resume</h2>
+			<Link to='../Magley-Resume.pdf' download target='_blank'>
+				Click to download
+			</Link>
+		</div>
+	);
 }
 
 export default Resume;
