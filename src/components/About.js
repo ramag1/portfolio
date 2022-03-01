bca91bcbd465c1c87d3f2e013dc6c924a8673af4
rmagley <rachelmagley@gmail.com>
2022-03-01 16:30:26 -0500

    Add about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import './About.css'
 
 function About(props) {
-	return <div>About Me</div>;
+	return (
+		<div>
+			<h2>About Me</h2>
+			<p className='about__p'>
+				Certified Fullstack Software Engineer with a robust background in
+				strategy development. <br />
+				<br />
+				Through my 10+ years of corporate retail experience, I possess a unique
+				viewpoint and approach to programming within the total business structure. <br />
+				My thought process is strategic and robust, while keeping efficiency top
+				of mind. <br />
+				Programming allows me to leverage this high-level, unique skill set to
+				delve into coding opportunities and toggle between big picture and small
+				details to deliver thoughtful results. <br />
+				<br />
+				Outside of coding, you can usually find me underwater leading a group of
+				divers across a reef or at a wreck. <br />
+				As a certified Divemaster, I've been able to take my diving skills to
+				the next level through 200+ dives all over the world.
+			</p>
+		</div>
+	);
 }
 
 export default About;
